425b81d267a00cabd74b529a95bb8ccaa9f8f3d5
Calvin Lee <lclee3@illinois.edu>
2022-11-01 20:34:52 -0500

    Fixed existing bug in the array.sv given to us

--- mp4/hdl/cache/array.sv
+++ mp4/hdl/cache/array.sv
@@ -9,18 +9,18 @@ module array #(parameter width = 1)
   output logic [width-1:0] dataout
 );
 
-//logic [width-1:0] data [2:0] = '{default: '0};
-logic [width-1:0] data [8];
-initial begin
-  data[0] = 0;
-  data[1] = 0;
-  data[2] = 0;
-  data[3] = 0;
-  data[4] = 0;
-  data[5] = 0;
-  data[6] = 0;
-  data[7] = 0;
-end
+logic [width-1:0] data [8] = '{default: '0};
+// logic [width-1:0] data [8];
+// initial begin
+//   data[0] = 0;
+//   data[1] = 0;
+//   data[2] = 0;
+//   data[3] = 0;
+//   data[4] = 0;
+//   data[5] = 0;
+//   data[6] = 0;
+//   data[7] = 0;
+// end
 
 always_comb begin
   dataout = (load  & (rindex == windex)) ? datain : data[rindex];

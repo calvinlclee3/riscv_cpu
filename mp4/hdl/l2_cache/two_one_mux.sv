7511c5c21e3d7e73f89337a52330c41668c87436
avikram2 <avikram2@illinois.edu>
2022-11-08 19:51:43 -0600

    Add new file

---
+++ mp4/hdl/l2_cache/two_one_mux.sv
@@ -0,0 +1,21 @@
+
+module two_one_mux
+(
+input logic selection, 
+input logic A, 
+input logic B,
+output logic dataout
+);
+
+always_comb begin
+    dataout = 1'b0;
+
+    case(selection)
+    1'b0: dataout = A;
+    1'b1: dataout = B;
+    endcase
+
+end
+
+
+endmodule

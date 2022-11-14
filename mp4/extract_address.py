7353e290f69d8635008b082270a5d5627c6b2dcb
Ayush Vikram <avikram2@illinois.edu>
2022-11-14 16:30:37 -0600

    finished with workflow to extract distinct address

---
+++ mp4/extract_address.py
@@ -0,0 +1,18 @@
+from collections import Counter
+import sys
+
+def parse_data(filename):
+    with open(filename) as fp:
+        lines = fp.readlines()	
+    filtered_lines = [line[line.rfind("=")+1:].strip() for line in lines if line.strip().startswith("a_pmem_address")]
+    
+    address_counter = Counter(filtered_lines)
+
+    distinct_addresses = [key for key in address_counter.keys() if key != "x" and key != "0"]
+
+
+    print("Number of total addressess (including 0 and x) is {}".format(len(filtered_lines)))    
+
+    print("Number of distinct non-zero and non-x addresses is {}".format(len(distinct_addresses)))
+if __name__ == "__main__":
+    parse_data(sys.argv[1])

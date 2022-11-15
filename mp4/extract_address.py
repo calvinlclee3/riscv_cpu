d0cd8175242d81c300292cb2ca82bdd7a16d3dd7
avikram2 <avikram2@illinois.edu>
2022-11-15 03:32:30 -0600

    Update extract_address.py

--- mp4/extract_address.py
+++ mp4/extract_address.py
@@ -1,18 +1,25 @@
 from collections import Counter
 import sys
 
+TAG_BITS = 24
+INDEX_BITS = 3
+
+ZERO = "00000000000000000000000000000000"
+
 def parse_data(filename):
     with open(filename) as fp:
         lines = fp.readlines()	
     filtered_lines = [line[line.rfind("=")+1:].strip() for line in lines if line.strip().startswith("a_pmem_address")]
-    filtered_lines = [int(addr) for addr in filtered_lines if addr != "0" and addr != "x"]
+    filtered_lines = [int(addr[31:31-TAG_BITS-INDEX_BITS]) for addr in filtered_lines if addr != ZERO and addr != "0" and addr.contains("x") == False]
+    #careful, python indexing doesn't include the end index (unlike sys verilog) ;)
 
     address_counter = Counter(filtered_lines)
 
-    distinct_addresses = [key for key in address_counter.keys() if key != 0]
+    distinct_addresses = [key for key in address_counter.keys() if key != 0]  
 
-    print("Number of total addressess (excluding 0 and x) is {}".format(len(filtered_lines)))    
+    print("Number of distinct non-zero and non-x addresses, which resolve to the same cache line, is {}".format(len(distinct_addresses)))
 
-    print("Number of distinct non-zero and non-x addresses is {}".format(len(distinct_addresses)))
+
+
 if __name__ == "__main__":
     parse_data(sys.argv[1])

a3039cc0cc9765f98d50019d4cc6b97127757955
avikram2 <avikram2@illinois.edu>
2022-11-15 03:36:33 -0600

    Update extract_address.py

--- mp4/extract_address.py
+++ mp4/extract_address.py
@@ -8,7 +8,7 @@ def parse_data(filename):
     with open(filename) as fp:
         lines = fp.readlines()	
     filtered_lines = [line[line.rfind("=")+1:].strip() for line in lines if line.strip().startswith("a_pmem_address")]
-    filtered_lines = [int(addr[31:31-TAG_BITS-INDEX_BITS]) for addr in filtered_lines if addr.contains("x") == False]
+    filtered_lines = [int(addr[31:31-TAG_BITS-INDEX_BITS]) for addr in filtered_lines if 'x' not in addr]
     #careful, python indexing doesn't include the end index (unlike sys verilog) ;)
 
     address_counter = Counter(filtered_lines)

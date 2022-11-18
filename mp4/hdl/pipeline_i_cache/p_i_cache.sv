393cba564cedfeecffe1d09a024aab26480cf86d
avikram2 <avikram2@illinois.edu>
2022-11-18 01:37:31 -0600

    Update p_i_cache.sv

--- mp4/hdl/pipeline_i_cache/p_i_cache.sv
+++ mp4/hdl/pipeline_i_cache/p_i_cache.sv
@@ -36,10 +36,82 @@ logic [255:0] mem_wdata;
 logic [255:0] mem_rdata;
 logic [31:0] mem_byte_enable;
 
+logic v_array_0_dataout;
+logic v_array_1_dataout;
+logic v_array_2_dataout;
+logic v_array_3_dataout;
+
+logic [2:0] LRU_array_dataout;
+
+logic v_array_0_load;
+logic v_array_0_datain;
+logic v_array_1_load;
+logic v_array_1_datain;
+logic v_array_2_load;
+logic v_array_2_datain;
+logic v_array_3_load;
+logic v_array_3_datain;
+
+logic tag_array_0_load;
+logic tag_array_1_load;
+logic tag_array_2_load;
+logic tag_array_3_load;
+
 i_cache_pipeline_reg cache_pipeline_in;
 i_cache_pipeline_reg cache_pipeline_out;
 
-p_i_cache_control control(.*);
+p_i_cache_control control(
+  .clk,
+  .rst,
+
+  /* CPU memory signals */
+  .mem_read,
+  .mem_resp,
+
+  /* Physical memory signals */
+  .pmem_resp,
+  .pmem_read,
+
+  /* Datapath to Control */
+  .v_array_0_dataout,
+  .v_array_1_dataout,
+  .v_array_2_dataout,
+  .v_array_3_dataout,
+
+  .cache_pipeline_out,
+  .cache_pipeline_in,
+
+  .LRU_array_dataout,
+
+  /* Control to Datapath */
+  output logic v_array_0_load,
+  output logic v_array_0_datain,
+  output logic v_array_1_load,
+  output logic v_array_1_datain,
+  output logic v_array_2_load,
+  output logic v_array_2_datain,
+  output logic v_array_3_load,
+  output logic v_array_3_datain,
+
+  output logic tag_array_0_load,
+  output logic tag_array_1_load,
+  output logic tag_array_2_load,
+  output logic tag_array_3_load,
+
+  output logic LRU_array_load,
+  output logic [2:0] LRU_array_datain,
+
+  output dataarraymux_sel_t write_en_0_MUX_sel,
+  output dataarraymux_sel_t write_en_1_MUX_sel,
+  output dataarraymux_sel_t write_en_2_MUX_sel,
+  output dataarraymux_sel_t write_en_3_MUX_sel,
+  output dataarraymux_sel_t data_array_0_datain_MUX_sel,
+  output dataarraymux_sel_t data_array_1_datain_MUX_sel,
+  output dataarraymux_sel_t data_array_2_datain_MUX_sel,
+  output dataarraymux_sel_t data_array_3_datain_MUX_sel,
+
+  output paddressmux_sel_t address_mux_sel,
+);
 
 
 //STAGE 1: COMPARE TAG AND SEE IF HIT OR MISS

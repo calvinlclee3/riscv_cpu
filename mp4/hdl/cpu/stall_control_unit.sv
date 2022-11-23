cbfee4aeb180f61221b73ae23c802f57c08fc04b
avikram2 <avikram2@illinois.edu>
2022-11-22 22:47:31 -0600

    Update stall_control_unit.sv

--- mp4/hdl/cpu/stall_control_unit.sv
+++ mp4/hdl/cpu/stall_control_unit.sv
@@ -106,6 +106,14 @@ always_comb
 begin
     set_defaults();
 
+    if(~instr_mem_resp)
+    begin
+        load_pc = 1'b0;
+        pipeline_load(1'b0, 1'b0, 1'b0, 1'b0);
+        global_stall = 1'b1;
+        //continue_i_cache = 1'b1;
+    end
+
     /* (Need CMP/ADDR Adder) after (Need ALU) */
     if((id_ex_out_ctrl.opcode == op_reg && arith_funct3_t'(id_ex_out_ctrl.funct3) != slt)  ||
        (id_ex_out_ctrl.opcode == op_reg && arith_funct3_t'(id_ex_out_ctrl.funct3) != sltu) ||
@@ -124,7 +132,7 @@ begin
                 load_pc = 1'b0;
                 pipeline_load(1'b0, 1'b1, 1'b1, 1'b1);
                 pipeline_flush(1'b0, 1'b1, 1'b0, 1'b0);
-                //continue_i_cache = 1'b0;
+                continue_i_cache = 1'b0;
             end
         end
 
@@ -135,7 +143,7 @@ begin
                 load_pc = 1'b0;
                 pipeline_load(1'b0, 1'b1, 1'b1, 1'b1);
                 pipeline_flush(1'b0, 1'b1, 1'b0, 1'b0);
-                //continue_i_cache = 1'b0;
+                continue_i_cache = 1'b0;
             end
         end
 
@@ -157,7 +165,7 @@ begin
                 load_pc = 1'b0;
                 pipeline_load(1'b0, 1'b1, 1'b1, 1'b1);
                 pipeline_flush(1'b0, 1'b1, 1'b0, 1'b0);
-                //continue_i_cache = 1'b0;
+                continue_i_cache = 1'b0;
             end
         end
 
@@ -349,16 +357,6 @@ begin
                 increment_tournament_pht = 1'b1;
         end
     end 
-
-
-    /* Instruction Cache Miss */ 
-    if(~instr_mem_resp)
-    begin
-        load_pc = 1'b0;
-        pipeline_load(1'b0, 1'b0, 1'b0, 1'b0);
-        global_stall = 1'b1;
-        //continue_i_cache = 1'b1;
-    end
 
     /* Data Cache Miss */
     if(data_mem_resp == 1'b0 && (ex_mem_out_ctrl.mem_read == 1'b1 || ex_mem_out_ctrl.mem_write == 1'b1))

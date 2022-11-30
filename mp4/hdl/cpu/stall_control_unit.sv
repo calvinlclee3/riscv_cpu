b493e2fa84beea8eee82a3a725df2c81848d5ee7
avikram2 <avikram2@illinois.edu>
2022-11-30 10:30:36 -0600

    Update stall_control_unit.sv

--- mp4/hdl/cpu/stall_control_unit.sv
+++ mp4/hdl/cpu/stall_control_unit.sv
@@ -370,7 +370,16 @@ begin
         global_stall = 1'b1;
         continue_i_cache = 1'b0;
 
-        if ((id_ex_out_ctrl.opcode == op_reg || id_ex_out_ctrl.opcode == op_imm) && (id_ex_out_ctrl.rd_id != ex_mem_out_ctrl.rd_id) && (id_ex_out_ctrl.rs1_id != ex_mem_out_ctrl.rd_id && id_ex_out_ctrl.rs2_id != ex_mem_out_ctrl.rd_id))
+        if (id_ex_out_ctrl.load_regfile == 1'b0)
+        begin
+            leap = 1'b1;
+            load_pc = 1'b1;
+            pipeline_load(1'b1, 1'b1, 1'b0, 1'b1);
+            global_stall = 1'b0;
+            continue_i_cache = 1'b1;
+        end
+
+        else if ((id_ex_out_ctrl.opcode == op_reg || id_ex_out_ctrl.opcode == op_imm) && (id_ex_out_ctrl.rd_id != ex_mem_out_ctrl.rd_id) && (id_ex_out_ctrl.rs1_id != ex_mem_out_ctrl.rd_id && id_ex_out_ctrl.rs2_id != ex_mem_out_ctrl.rd_id))
         begin
             leap = 1'b1;
             load_pc = 1'b1;

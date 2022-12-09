b0a0de095ba2ba2c427ad586f39f39262653efa5
avikram2 <avikram2@illinois.edu>
2022-12-08 19:39:02 -0600

    Revert "Update array.sv"
    This reverts commit f03b3c7f95225a9e0c251792e7bdbb2d515fb480

--- mp4/hdl/cache/array.sv
+++ mp4/hdl/cache/array.sv
@@ -1,15 +1,16 @@
+
 module array #(parameter width = 1)
 (
   input clk,
   input rst,
   input logic load,
-  input logic [3:0] rindex,
-  input logic [3:0] windex,
+  input logic [2:0] rindex,
+  input logic [2:0] windex,
   input logic [width-1:0] datain,
   output logic [width-1:0] dataout
 );
 
-logic [width-1:0] data [16];
+logic [width-1:0] data [8];
 
 always_comb begin
   dataout = (load  & (rindex == windex)) ? datain : data[rindex];
@@ -19,7 +20,7 @@ always_ff @(posedge clk)
 begin
     if(rst)
     begin
-      for (int i = 0; i < 16; ++i)
+      for (int i = 0; i < 8; ++i)
         data[i] <= '0;
     end
     else if(load)
